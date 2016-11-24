581daa4c2a4468d7a898a422280530bcb1deb48c
Alec Brunelle <alec@alec.coffee>
2016-11-24 15:37:35 -0500

    added half of part 1.4

--- Mutation.hs
+++ Mutation.hs
@@ -19,51 +19,75 @@ data Value = IntVal Integer |
              BoolVal Bool
              deriving Show
 
+
 -- A type representing a container for stored "mutable" values.
 type Memory = AList Integer Value
 
 -- A type representing a pointer to a location in memory.
 data Pointer a = P Integer
 
+-- Question 4
+
+data StateOp a = StateOp (Memory -> (a, Memory))
+
+runOp :: StateOp a -> Memory -> (a, Memory)
+runOp (StateOp op) mem = op mem
+
+--
 
 -- Type class representing a type which can be stored in "Memory".
 class Mutable a where
     -- Look up a value in memory referred to by a pointer.
-    get :: Memory -> Pointer a -> a
+    -- get :: Memory -> Pointer a -> a
+    get :: Pointer a -> StateOp a
 
     -- Change a value in memory referred to by a pointer.
     -- Return the new memory after the update.
-    set :: Memory -> Pointer a -> a -> Memory
+    -- set :: Memory -> Pointer a -> a -> Memory
+    set :: Pointer a -> a -> StateOp a
 
     -- Create a new memory location storing a value, returning a new pointer
     -- and the new memory with the new value.
     -- Raise an error if the input Integer is already storing a value.
-    def :: Memory -> Integer -> a -> (Pointer a, Memory)
+    -- def :: Memory -> Integer -> a -> (Pointer a, Memory)
+    def :: Integer -> a -> StateOp a
 
 -- Question 2
 
 get_bool (BoolVal value) = value
 
 instance Mutable Bool where
-    get mem (P pointer_val) = if inA mem pointer_val then (get_bool (lookupA mem pointer_val))
-                              else error "pointer value not in memory!"
-    set mem (P pointer_val) value = updateA mem (pointer_val, (BoolVal value))
-    def mem pointer_val value = if inA mem pointer_val then error "Already in Memory!"
+    get (P pointer_val) = (StateOp (\mem -> (if inA mem pointer_val then (get_bool (lookupA mem pointer_val))
+                                             else error "pointer value not in memory!", mem)))
+    set (P pointer_val) value = (StateOp
+                                  (\mem -> (value, updateA mem (pointer_val, (BoolVal value))))
+                                )
+    def pointer_val value = (StateOp
+                              (\mem ->
+                                if inA mem pointer_val then error "Already in Memory!"
                                 else
                                 (
-                                  (P pointer_val),
+                                  value,
                                   insertA mem (pointer_val, (BoolVal value))
                                 )
+                              )
+                            )
 
 get_integer (IntVal value) = value
 
 instance Mutable Integer where
-  get mem (P pointer_val) = if inA mem pointer_val then (get_integer (lookupA mem pointer_val))
-                           else error "pointer value not in memory!"
-  set mem (P pointer_val) value = updateA mem (pointer_val, (IntVal value))
-  def mem pointer_val value = if inA mem pointer_val then error "Already in Memory!"
+  get (P pointer_val) = (StateOp (\mem -> (if inA mem pointer_val then (get_integer (lookupA mem pointer_val))
+                                           else error "pointer value not in memory!", mem)))
+  set (P pointer_val) value = (StateOp
+                                (\mem -> (value, updateA mem (pointer_val, (IntVal value))))
+                              )
+  def pointer_val value = (StateOp
+                            (\mem ->
+                              if inA mem pointer_val then error "Already in Memory!"
                               else
                               (
-                                (P pointer_val),
+                                value,
                                 insertA mem (pointer_val, (IntVal value))
                               )
+                            )
+                          )
