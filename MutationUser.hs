c4c79d487aa2efe7206575d167724c0755f0efce
Alec Brunelle <alec@alec.coffee>
2016-11-28 22:14:48 -0500

    part 3 done

--- MutationUser.hs
+++ MutationUser.hs
@@ -4,7 +4,8 @@ This file contains code which uses the mutation library found in Mutation.hs
 -}
 module MutationUser (
   pointerTest,
-  swap
+  swap,
+  swapCycle
   )
   where
 
@@ -39,24 +40,25 @@ pointerTest value = (StateOp (\mem -> let (StateOp f) = (def 100 (value + 3))
 -- Question 6
 -- takes two pointers and swaps the values they refer to.
 swap :: Mutable a => Pointer a -> Pointer a -> StateOp ()
--- swap a b = (StateOp (\mem -> let (StateOp f) = get a
---                                  (x, s1) = f mem
---                                  (StateOp g) = get b
---                                  (y, s2) = g s1
---                                  (StateOp k) = set a x >>> set b y
---                              in ((), (snd (k mem)))
---
---               )
---             )
-swap a b = get b >~> (\value -> set a value) >>>
-           get a >~> (\value -> set b value) >>>
-           StateOp (\mem -> ((), mem))
+swap a b = (StateOp (\mem -> let (StateOp f) = get a
+                                 (a_value, newMem) = f mem
+                                 (StateOp g) = get b >~> (\b_value -> set a b_value)
+                                 (y, newMem2) = g mem
+                                 (StateOp h) = set b a_value
+                                 (k, newMem3) = h newMem2
+                             in ((), newMem3)
 
+              )
+            )
 
--- swap :: Mutable a => Pointer a -> Pointer a -> StateOp ()
--- swap a b = let (StateOp x) = get a
--- 					     (StateOp y) = get b
--- 			in StateOp (\mem -> ((), mem))
+-- takes a list of pointers p1,
+-- ..., pn, with corresponding values v1, ..., vn, and sets p1's value to v2, p2's value to v3.,
+-- etc., and pn's value to v1. This function should not change anything if its argument has length
+-- less than 2.
+helper [] = StateOp (\mem -> ((), mem))
+helper [x] = StateOp (\mem -> ((), mem))
+helper (x:y:xs) = swap x y >>> helper (y:xs)
 
--- swapCycle :: Mutable a => [Pointer a] -> StateOp ()
--- swapCycle = undefined
+swapCycle :: Mutable a => [Pointer a] -> StateOp ()
+swapCycle pointer_list = if (length pointer_list < 3) then StateOp (\mem -> ((), mem))
+                         else helper pointer_list
